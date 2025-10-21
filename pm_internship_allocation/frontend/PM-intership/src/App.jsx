bb482e181d2139eb1dcfd8a67fe67d78cb07fdd2
siva-web-15 <muthusivabalan2007@gmail.com>
2025-10-21 16:35:00 +0530

    improve code functionality

--- pm_internship_allocation/frontend/PM-intership/src/App.jsx
+++ pm_internship_allocation/frontend/PM-intership/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import IndustryPortal from './components/portals/IndustryPortal';
